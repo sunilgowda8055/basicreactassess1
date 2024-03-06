13470403773cd237407844e118864d5af2f7ac69
Sunil Gowda <sunilcgowda8055@gmail.com>
2024-03-06 17:52:09 +0530

    new changes

---
+++ src/components/MobileOS.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function MobileOS() {
+  return (
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h2>Mobile Manufacturers</h2>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MobileOS
